bddbd105e7aba229bf1a2b4b3c1d0e9db6c67aa1
Michelle Child <>
2017-11-14 22:07:10 -0500

    got slick.js working! css issue

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,31 +1,18 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
-import slick from 'slick-carousel';
+
+import 'slick-carousel/slick/slick';
 
 window.$ = $;
 
 import Foundation from 'foundation-sites';
+
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-import src from 'owl.carousel'; // seems like 'import dist' also works
 
 $(document).foundation();
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+$('.js-slick').slick({
+  dots: true
 });
